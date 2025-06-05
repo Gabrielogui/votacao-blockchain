f6b911f1da1a360718ce2a0ef0bd408ea447e297
Gabrielogui <gabrielcsro@gmail.com>
2025-06-05 09:46:30 -0300

    server.py atualizado + node.proto

--- server/server.py
+++ server/server.py
@@ -1,14 +1,22 @@
 from concurrent import futures
 import grpc
+
 import voting_pb2_grpc
-from node import VotingNode
+import node_pb2_grpc
+from node import VotingNode, NodeService
+
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+
     voting_pb2_grpc.add_VotingServiceServicer_to_server(VotingNode(), server)
+    node_pb2_grpc.add_NodeServiceServicer_to_server(NodeService(node_id="Node-1"), server)
+
     server.add_insecure_port('[::]:50051')
     server.start()
+    print("Servidor rodando na porta 50051...")
     server.wait_for_termination()
+
 
 if __name__ == '__main__':
     serve()
